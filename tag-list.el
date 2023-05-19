95ca5a3f6f103529a172c9c454ae083393fabaac
Vinicius <vinicamilotti@gmail.com>
2023-05-19 19:14:37 -0300

    List of links working as inteded

--- tag-list.el
+++ tag-list.el
@@ -11,35 +11,23 @@
 ;; In test
 
 ;;; Code:
-(require 'org-roam)
-(defun my/org-roam-filter-by-tag (tag-name)
-  (lambda (node)
-    (member tag-name (org-roam-node-tags node))))
-
-
-(defun my/org-roam-list-notes-by-tag (tag-name)
-  (mapcar #'org-roam-node-file
-          (seq-filter
-           (my/org-roam-filter-by-tag tag-name)
-           (org-roam-node-list))))
 
 (defun linksByTag(tag)
   "This function gets all the org-roam files with matching tags (TAG)."
-  (defvar roam/allnotes (my/org-roam-list-notes-by-tag tag))
-  (lambda (list)
-      (let (value)
+  (setq-local list (my/org-roam-list-notes-by-tag tag))
+  (sort (let (value)
       (dolist (element list value)
 	(message "%s" element)
-	(defvar rawTitle (org-roam-db-query [:select TITLE :from nodes :where (= FILE $s1) ] element))
-	(defvar rawId (org-roam-db-query [:select ID :from nodes :where (= FILE $s1)] element))
-	(defvar title (car rawTitle))
-	(defvar id (car rawId))
-	(defvar toWrite (format "[[id:%s][%s]]" (car id) (car title)))
+	(setq-local rawTitle (org-roam-db-query [:select TITLE :from nodes :where (= FILE $s1) ] element))
+	(setq-local rawId (org-roam-db-query [:select ID :from nodes :where (= FILE $s1)] element))
+	(setq-local title (car rawTitle))
+	(setq-local id (car rawId))
+	(setq-local toWrite (format "[[id:%s][%s]]" (car id) (car title)))
 	(message "%s" toWrite)
 	(setq value (cons toWrite value))
 	)
-      ))
-)
+      ) 'a)
+  )
+(message "%s" (linksByTag "Git"))
 (provide 'tag-list)
 ;;; tag-list.el ends here
-
