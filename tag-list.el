1e9a8fdbefe6363f155746d05d891d77e0324704
Vinicius <vinicamilotti@gmail.com>
2023-05-19 18:22:56 -0300

    somethings changed

--- tag-list.el
+++ tag-list.el
@@ -2,7 +2,6 @@
 ;;; Commentary:
 ;;;; In test
 ;;; Code:
-(require 'org-roam-mode)
 (defun my/org-roam-filter-by-tag (tag-name)
   (lambda (node)
     (member tag-name (org-roam-node-tags node))))
@@ -14,18 +13,18 @@
            (my/org-roam-filter-by-tag tag-name)
            (org-roam-node-list))))
 
-(defun getAllByTag(tag)
+(defun linksByTag(tag)
   "This function gets all the org-roam files with matching tags (TAG)."
   (defvar roam/allnotes (my/org-roam-list-notes-by-tag tag))
   (lambda (list)
       (let (value)
       (dolist (element list value)
 	(message "%s" element)
-	(setq rawTitle (org-roam-db-query [:select TITLE :from nodes :where (= FILE $s1) ] element))
-	(setq rawId (org-roam-db-query [:select ID :from nodes :where (= FILE $s1)] element))
-	(setq title (car rawTitle))
-	(setq id (car rawId))
-	(setq toWrite (format "[[id:%s][%s]]" (car id) (car title))) 
+	(defvar rawTitle (org-roam-db-query [:select TITLE :from nodes :where (= FILE $s1) ] element))
+	(defvar rawId (org-roam-db-query [:select ID :from nodes :where (= FILE $s1)] element))
+	(defvar title (car rawTitle))
+	(defvar id (car rawId))
+	(defvar toWrite (format "[[id:%s][%s]]" (car id) (car title)))
 	(message "%s" toWrite)
 	(setq value (cons toWrite value))
 	)
