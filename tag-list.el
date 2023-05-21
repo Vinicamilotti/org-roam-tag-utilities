841ab78c9c53ab851afa58df7bcb6c48d2633684
Vinicius <vinicamilotti@gmail.com>
2023-05-21 15:54:29 -0300

    All main features working

--- tag-list.el
+++ tag-list.el
@@ -11,23 +11,65 @@
 ;; In test
 
 ;;; Code:
+(defun my/org-roam-filter-by-tag (tag-name)
+    "Filter (NODE)s by (TAG-NAME)."
+  (lambda (node)
+    (member tag-name (org-roam-node-tags node))))
 
-(defun linksByTag(tag)
+(defun my/org-roam-list-notes-by-tag (tag-name)
+  "List notes by (TAG-NAME)."
+  (mapcar #'org-roam-node-file
+          (seq-filter
+           (my/org-roam-filter-by-tag tag-name)
+           (org-roam-node-list))))
+
+(defun org-roam-links-by-tag(tag)
   "This function gets all the org-roam files with matching tags (TAG)."
-  (setq-local list (my/org-roam-list-notes-by-tag tag))
-  (sort (let (value)
+  (setq list (my/org-roam-list-notes-by-tag tag))
+  (let (value)
       (dolist (element list value)
-	(message "%s" element)
-	(setq-local rawTitle (org-roam-db-query [:select TITLE :from nodes :where (= FILE $s1) ] element))
-	(setq-local rawId (org-roam-db-query [:select ID :from nodes :where (= FILE $s1)] element))
-	(setq-local title (car rawTitle))
-	(setq-local id (car rawId))
-	(setq-local toWrite (format "[[id:%s][%s]]" (car id) (car title)))
-	(message "%s" toWrite)
+	(setq rawTitle (org-roam-db-query [:select TITLE :from nodes :where (= FILE $s1) ] element))
+	(setq rawId (org-roam-db-query [:select ID :from nodes :where (= FILE $s1)] element))
+	(setq title (car rawTitle))
+	(setq id (car rawId))
+	(setq toWrite (format "[[id:%s][%s]]" (car id) (car title)))
 	(setq value (cons toWrite value))
 	)
-      ))
+      )
   )
-(message "%s" (linksByTag "Git"))
+
+(defun org-roam-tag-search(tags)
+  "Search notes via list of tags (TAGS)."
+  (let (value)
+    (dolist (element tags value)
+	    (setq search (my/org-roam-list-notes-by-tag element))
+	    (setq value (cons search value))
+	    ))
+	)
+(defun org-roam-matching-tags-search(tags)
+  "Return a list of files with matching tags (TAGS)."
+ (setq files (org-roam-tag-search tags))
+ (let ((result (car files)))
+      (dolist (lst (cdr files) result)
+        (setq result (cl-intersection result lst)))))
+
+(defun org-roam-links-by-matching-tags(tags)
+  "Return a list of links based on a list of (TAGS)."
+  (setq files (org-roam-matching-tags-search tags))
+  (let (value)
+      (dolist (element files value)
+	(setq rawTitle (org-roam-db-query [:select TITLE :from nodes :where (= FILE $s1) ] element))
+	(setq rawId (org-roam-db-query [:select ID :from nodes :where (= FILE $s1)] element))
+	(setq title (car rawTitle))
+	(setq id (car rawId))
+	(setq toWrite (format "[[id:%s][%s]]" (car id) (car title)))
+	(setq value (cons toWrite value))
+	)
+      )
+  )
+
+(mesage "%s" (org-roam-links-by-matching-tags '("Git" "Santandercoders")))
+
 (provide 'tag-list)
+
 ;;; tag-list.el ends here
